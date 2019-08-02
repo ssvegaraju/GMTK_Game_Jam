25695f5117fb2fa16e794da626e8132325e6f70e
tayo123 <tfalase@gmail.com>
2019-08-02 15:06:46 -0700

    Snap to gear.

---
+++ Assets/Scripts/SnapToPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToPosition : MonoBehaviour
+{
+    public GameObject objectToSnap;
+    public GameObject objectToSnapTo;
+    private bool snapped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(objectToSnap.gameObject.transform.position, gameObject.transform.position);
+        if (distance < 0.5f)
+        {
+            snapped = true;
+            objectToSnap.transform.position = objectToSnapTo.transform.position;
+            objectToSnap.transform.rotation = objectToSnapTo.transform.rotation;
+        }
+
+        if (snapped)
+        {
+            objectToSnap.transform.position = objectToSnapTo.transform.position;
+            objectToSnap.transform.rotation = objectToSnapTo.transform.rotation;
+        }
+    }
+}
