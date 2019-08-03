d06b4a7460c76d9d7ac0ae5af4115cd9684e0002
Sam Long <sam3217@uw.edu>
2019-08-02 18:57:28 -0700

    Add changes

---
+++ Assets/MoveDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateDoor() {
+
+    }
+}
