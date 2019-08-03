7e7f18545b8b8b4f88fa46b37e337332400d1d05
Sukeerth <ssvegaraju@gmail.com>
2019-08-03 13:13:12 -0700

    Finally done with main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public MenuButton[] buttons;
+
+    private int currentIndex = 0;
+
+    private float inputDelay = 0.3f;
+    private float lastInputTime;
+
+    // Start is called before the first frame update
+    void Start() {
+        buttons[0].OnSelectedButton();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (buttons == null)
+            return;
+        float input = Input.GetAxisRaw("Vertical");
+        if (Mathf.Abs(input) > 0.1f && Time.time - lastInputTime > inputDelay) {
+            lastInputTime = Time.time;
+            int newIndex = (input < 0) ? currentIndex + 1 : currentIndex - 1;
+            if (newIndex >= buttons.Length) {
+                newIndex = 0;
+            }
+            if (newIndex < 0) {
+                newIndex = buttons.Length - 1;
+            }
+            ChangeSelection(buttons[currentIndex], buttons[newIndex]);
+            currentIndex = newIndex;
+        }
+        if (Input.GetButtonDown("Submit")) {
+            buttons[currentIndex].OnPressedButton();
+        }
+    }
+
+    private void ChangeSelection(MenuButton old, MenuButton current) {
+        old.OnUnselectedButton();
+        current.OnSelectedButton();
+    }
+
+    public void StartNewGame() {
+        StartCoroutine(LoadScene());
+    }
+
+    private IEnumerator LoadScene() {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync("Level 1");
+    }
+
+    public void Options() {
+
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
