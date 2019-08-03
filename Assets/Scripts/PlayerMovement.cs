fbbfd31f32c53bb5d262dd15f5d3efdedab84ef9
Sukeerth <ssvegaraju@gmail.com>
2019-08-02 20:34:07 -0700

    Added animations (not hooked up yet) and fixed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D)), RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float acceleration;
     [Range(1, 15)]
     public float moveSpeed = 8f;
-    [Range(0f, 1f)]
-    public float stopSpeed = 0.5f;
-    public float jumpForce;
+    public float jumpForce = 8f;
     public float unsnapReleaseForce = 10;
     public float rotateSpeed = 4f;
+    public float lowJumpMultiplier = 1.5f, fallMultiplier = 2.5f;
 
     private float horizontal;
     private bool vertical;
@@ -25,34 +23,34 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rigid;
     private Collider2D col;
 
+    private Vector3 moveDirection;
+
     private void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
     }
 
-    private void Update() {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetButtonDown("Jump");
-        rotate = Input.GetButton("Rotate");
-    }
-
     private void FixedUpdate()
     {
-        if (!isDead && !isSnapped) {
-            if (onGround && Mathf.Abs(horizontal) < 0.1f) {
-                rigid.velocity = Vector2.Lerp(rigid.velocity, Vector2.up * rigid.velocity.y, stopSpeed);
-            } else {
-                if (Mathf.Abs(rigid.velocity.x) < moveSpeed) 
-                    rigid.velocity += Vector2.right * horizontal * acceleration * Time.fixedDeltaTime;
-            }
-            if (rotate)
-                rigid.MoveRotation(transform.eulerAngles.z - rotateSpeed);
-            if (!onGround) {
-                rigid.velocity += Vector2.up * Physics2D.gravity.y * 2.5f * Time.fixedDeltaTime;
-            }
-            if (onGround && vertical) {
-                Jump();
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetButton("Jump");
+        rotate = Input.GetButton("Rotate");
+        if (!isDead) {
+            if (!isSnapped) {
+                moveDirection = Vector3.right * horizontal * moveSpeed;
+                if (rotate)
+                    rigid.MoveRotation(transform.eulerAngles.z - rotateSpeed);
+                if (onGround && vertical) {
+                    Jump();
+                } else {
+                    moveDirection.y = rigid.velocity.y;
+                }
+                rigid.velocity = moveDirection;
+                if (rigid.velocity.normalized.y < 0)
+                    rigid.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
+                else if (rigid.velocity.normalized.y > 0 && !vertical)
+                    rigid.velocity += Vector2.up * Physics2D.gravity.y * lowJumpMultiplier * Time.deltaTime;
             }
         } else {
             rigid.MoveRotation(transform.eulerAngles.z - rotateSpeed);
@@ -74,9 +72,8 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Jump() {
-        rigid.velocity = Vector2.up * jumpForce;
+        moveDirection.y = jumpForce;
         onGround = false;
-        rigid.angularVelocity = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
