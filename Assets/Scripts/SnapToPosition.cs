8351ebb3d91ead0fb621ed0d1d3da5114d4a37ee
Sukeerth <ssvegaraju@gmail.com>
2019-08-02 18:40:44 -0700

    fixed collision with ground bug, and edited snapping

--- Assets/Scripts/SnapToPosition.cs
+++ Assets/Scripts/SnapToPosition.cs
@@ -5,53 +5,79 @@ using UnityEngine.Events;
 
 public class SnapToPosition : MonoBehaviour
 {
+    public float detectionRadius;
+    public Vector3 offset;
     public PlayerMovement objectToSnap;
     public GameObject objectToSnapTo;
-    public static bool snapped = false;
+    private bool snapped = false;
     private float vertical;
+
 
     public UnityEvent OnSnap, OnUnsnap;
 
-    private float snapTime, releaseTime;
+    private CircleCollider2D col;
     // Start is called before the first frame update
     void Start()
     {
+        col = GetComponent<CircleCollider2D>();
+        if (col == null)
+            col = gameObject.AddComponent<CircleCollider2D>();
+        col.isTrigger = true;
+        col.radius = detectionRadius;
         if (objectToSnap == null)
             objectToSnap = FindObjectOfType<PlayerMovement>();
+        OnSnap.AddListener(delegate () {
+            objectToSnap.OnSnap();
+        });
+        OnUnsnap.AddListener(delegate () {
+            objectToSnap.OnUnsnap();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
         vertical = Input.GetAxis("Vertical");
+    }
 
-        float distance = Vector2.Distance(objectToSnap.transform.position, gameObject.transform.position);
-        if (distance < 0.5f && !snapped && Time.time - releaseTime > 0.3f)
-        {
-            snapped = true;
-            snapTime = Time.time;
-            if (OnSnap != null) {
-                OnSnap.Invoke();
-            }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (!snapped && collision.gameObject.CompareTag("Player")) {
+            Snap();
         }
+    }
 
+    private void Snap() {
         if (snapped)
-        {
-            if (Time.time - snapTime <= 0.3f || vertical < 0.1f)
-            {
-                objectToSnap.transform.position = objectToSnapTo.transform.position;
-                objectToSnap.transform.rotation = objectToSnapTo.transform.rotation;
-            }
-            else 
-            {
-                float dir = objectToSnapTo.transform.eulerAngles.z + 60;
-                objectToSnap.Unsnap(dir);
-                releaseTime = Time.time;
-                snapped = false;
-                if (OnUnsnap != null) {
-                    OnUnsnap.Invoke();
-                }
-            }
+            return;
+        snapped = true;
+        if (OnSnap != null) {
+            OnSnap.Invoke();
         }
+        Debug.Log("Snapped");
+        StartCoroutine(WhileSnapping());
+    }
+
+    private IEnumerator WhileSnapping() {
+        float startTime = Time.time;
+        while (snapped || Time.time - startTime < 0.5f) {
+            objectToSnap.transform.position = objectToSnapTo.transform.position;
+            objectToSnap.transform.rotation = objectToSnapTo.transform.rotation;
+            snapped = vertical < 0.1f;
+            yield return null;
+        }
+        Unsnap();
+    }
+
+    private void Unsnap() {
+        float dir = objectToSnapTo.transform.eulerAngles.z + 60;
+        objectToSnap.transform.eulerAngles = Vector3.forward * dir;
+        objectToSnap.transform.Translate(objectToSnap.transform.up.normalized * (detectionRadius + 0.3f));
+        snapped = false;
+        if (OnUnsnap != null) {
+            OnUnsnap.Invoke();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position + offset, detectionRadius);
     }
 }
