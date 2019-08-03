0971a56a6f4cb3e83fab1cacd64d47423acb121a
Sam Long <sam3217@uw.edu>
2019-08-02 22:40:53 -0700

    Add respawn, kind of.

---
+++ Assets/Scripts/CollectTriangles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectTriangles : MonoBehaviour
+{
+    private int collectedAmt;
+
+    public void Increment() {
+        collectedAmt++;
+        Debug.Log(collectedAmt);
+        if (collectedAmt > 3) {
+            GameObject.Find("Main Camera").GetComponent<CameraFollow>().ShakeScreen(3f, .7f);
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
